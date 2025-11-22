4a57486cfd176182c7516177f6594871673bd9bb
Jonasodiq <jonasniyazson@hotmail.com>
2025-11-22 14:27:39 +0100

    import UpdateCommand

--- functions/deleteNote/index.js
+++ functions/deleteNote/index.js
@@ -1,5 +1,5 @@
 const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
-const { DynamoDBDocumentClient, GetCommand, DeleteCommand } = require("@aws-sdk/lib-dynamodb");
+const { DynamoDBDocumentClient, GetCommand, DeleteCommand, UpdateCommand } = require("@aws-sdk/lib-dynamodb");
 const middy = require("middy");
 const { authMiddleware } = require("../../utils/middleware");
 const { success, error } = require("../../utils/responses");
@@ -30,10 +30,25 @@ const handler = async (event) => {
     }
 
     // Delete note
+    // await db.send(
+    //   new DeleteCommand({
+    //     TableName: "Notes",
+    //     Key: { userId, id }
+    //   })
+    // );
+
+    // Update
     await db.send(
-      new DeleteCommand({
+      new UpdateCommand({
         TableName: "Notes",
-        Key: { userId, id }
+        Key: { userId, id },
+        UpdateExpression: "SET #deleted = :trueVal",
+        ExpressionAttributeNames: {
+          "#deleted": "deleted"
+        },
+        ExpressionAttributeValues: {
+          ":trueVal": true
+        }
       })
     );
 
